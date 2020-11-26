e5ced6ea90372218749b5e2172b52b82de35c174
Kiëd Llaentenn <kiedtl@tilde.team>
2020-11-26 21:17:23 +0000

    coffee: tea: add

---
+++ mod/coffee.py
@@ -0,0 +1,56 @@
+import handlers
+import out
+import random
+from common import nohighlight
+
+modname = "coffee"
+
+ACTION = ["hands", "gives", "passes", "tosses", "throws"]
+ESPRESSO = ["mocha", "latte", "macchiato", "breve", "americano", "cubano", "cappuccino"]
+SIZES = ["small", "medium", "short", "tall", "large", "grande", "venti"]
+FLAVORS = [
+    "hazelnut",
+    "white chocolate",
+    "dark chocolate",
+    "caramel",
+    "vanilla",
+    "cinnamon",
+]
+HEAT = ["iced", "cold", "lukewarm", "warm", "hot", "boiling hot", "scalding"]
+
+
+def _espresso():
+    size = random.choice(SIZES)
+    heat = random.choice(HEAT)
+    flavor = random.choice(FLAVORS)
+    coffee = random.choice(ESPRESSO)
+    shots = round(random.uniform(1, 3))
+
+    if shots == 1:
+        shots_str = "shot"
+    else:
+        shots_str = "shots"
+
+    espresso = nohighlight("espresso")
+    return f"{size} {heat} {flavor} {coffee} with {shots} {shots_str} of {espresso}"
+
+
+async def serve(self, ch, src, msg, args, opts):
+    """
+    :name: coffee
+    :hook: cmd
+    :help: serve some coffee!
+    :args: @user:str
+    """
+    recipient = src
+    if len(msg) > 0:
+        recipient = msg
+
+    espresso = _espresso()
+    action = random.choice(ACTION)
+
+    await self.ctcp(ch, "ACTION", f"{action} {recipient} a {espresso}!")
+
+
+async def init(self):
+    handlers.register(self, modname, serve)
---
+++ mod/tea.py
@@ -0,0 +1,137 @@
+import dataclasses
+import handlers
+import out
+import random
+
+from common import nohighlight
+from typing import *
+
+modname = "tea"
+
+def _flatten(src):
+    return [item for sublist in src for item in sublist]
+
+
+CHANCE_OF_TEA_ADJ = 25
+CHANCE_OF_VESSEL_SIZE = 75
+CHANCE_OF_VESSEL_ADJ = 55
+
+ACTION = ["hands", "gives", "passes", "tosses", "throws"]
+
+
+@dataclasses.dataclass
+class Tea:
+    name: str
+    vessels: List[str]
+    adjs: List[str]
+    heats: List[List[str]]
+
+
+# fmt: off
+
+NEWMANS   = "Newman's Own"
+EARL      = "Earl Grey"
+FAIRTRADE = "fair trade"
+HOMEMADE  = "homemade"
+HOMEBREWN = "home-brewn"
+KANGRA    = "Kangra"
+IRISH     = "Irish"
+ENGLISH   = "English"
+
+COLD = ["iced", "cold", "chilled"]
+WARM = ["lukewarm", "warm", "warmish"]
+HOT  = ["boiling", "scalding", "steaming", "sweltering", "toasty hot"]
+ALL  = _flatten([COLD, WARM, HOT])
+
+TEAPOT  = ["teapot",  ["vintage", "silver", "English", "antique silver", "jasperware"]]
+MUG     = ["mug",     ["stoneware", "porcelain", "jasperware", "wooden"]]
+BOWL    = ["bowl",    ["burl wood-and-silver tea", "Tibetan tea", "Tibetan silver tea"]]
+SAMOVAR = ["samovar", ["antique", "vintage", "brass", "silver"]]
+TEACUP  = ["teacup",  []]
+HOHIN   = ["hohin",   []]
+GAIWAN  = ["gaiwan",  ["porcelain", "Ruyao"]]
+SHIBOR_ = ["shiboridashi",  ["porcelain", "Ruyao"]]
+
+SIZES = ["large", "small", "medium", "tall", "wide", "big", "100ml",
+        "giant", "tiny"]
+
+TEA = nohighlight("tea")
+
+TEAS = [
+    Tea(f"black {TEA}",                  [SAMOVAR, TEAPOT, MUG, TEACUP],
+        [IRISH, ENGLISH, NEWMANS, EARL], [ALL]),
+    Tea(f"green {TEA}",                  [TEAPOT, MUG, HOHIN], [KANGRA], [ALL]),
+    Tea(f"matcha green {TEA}",           [HOHIN, TEAPOT, MUG, TEACUP], [], [ALL]),
+    Tea(f"sencha green {TEA}",           [SHIBOR_, HOHIN, TEAPOT, MUG, TEACUP], [], [ALL]),
+    Tea(f"white {TEA}",                  [TEAPOT, MUG, TEACUP], [EARL], [ALL]),
+    Tea(f"oolong {TEA}",                 [TEAPOT, MUG, TEACUP], [], [WARM, HOT]),
+    Tea(f"pu'er",                        [SHIBOR_, TEAPOT, MUG, TEACUP], [], [ALL]),
+    Tea(f"chai",                         [TEAPOT, MUG, TEACUP], [HOMEMADE], [WARM, HOT]),
+    Tea(f"butter {TEA}",                 [TEAPOT, MUG, BOWL, TEACUP], [HOMEMADE], [HOT]),
+    Tea(f"christmas {TEA}",              [TEAPOT, MUG, TEACUP], [HOMEMADE], [ALL]),
+    Tea(f"rooibos {TEA}",                [TEAPOT, MUG, TEACUP], [], [ALL]),
+    Tea(f"tulsi {TEA}",                  [TEAPOT, MUG, TEACUP], [FAIRTRADE], [COLD, HOT]),
+    Tea(f"spearmint {TEA}",              [TEAPOT, MUG], [HOMEMADE], [COLD]),
+    Tea(f"peppermint {TEA}",             [TEAPOT, MUG], [HOMEMADE], [COLD]),
+    Tea(f"chocolate mint {TEA}",         [TEAPOT, MUG], [HOMEMADE], [COLD]),
+    Tea(f"mullein herbal {TEA}",         [MUG], [HOMEMADE], [ALL]),
+    Tea(f"lamb's ears herbal {TEA}",     [MUG], [HOMEMADE], [ALL]),
+    Tea(f"tumeric ginger {TEA}",         [TEAPOT, MUG, TEACUP], [NEWMANS],  [ALL]),
+    Tea(f"lemongrass-verbena {TEA}",     [MUG, TEACUP], [HOMEMADE], [COLD]),
+    Tea(f"black currant hibiscus {TEA}", [MUG, TEACUP], [], [ALL]),
+    Tea(f"roasted dandelion root {TEA}", [TEAPOT, MUG, TEACUP], [HOMEMADE], [ALL]),
+    Tea(f"lavender {TEA}",               [TEAPOT, MUG, TEACUP], [HOMEMADE], [ALL]),
+]
+
+# fmt: on
+
+
+def _tea():
+    tea = random.choice(TEAS)
+
+    heat = random.choice(_flatten(tea.heats))
+    vessel_type = random.choice(tea.vessels)
+    vessel = vessel_type[0]
+
+    vessel_str = ""
+    if random.uniform(0, 100) < CHANCE_OF_VESSEL_ADJ and len(vessel_type[1]) > 0:
+        size = ""
+        if random.uniform(0, 100) < CHANCE_OF_VESSEL_SIZE:
+            size = random.choice(SIZES) + " "
+
+        vessel_adj = random.choice(vessel_type[1])
+        vessel_str = f"{size}{vessel_adj} {vessel}"
+    else:
+        vessel_str = f"{vessel}"
+
+    tea_str = ""
+    if random.uniform(0, 100) < CHANCE_OF_TEA_ADJ and len(tea.adjs) > 0:
+        tea_adj = random.choice(tea.adjs)
+        tea_str = f"{tea_adj} {tea.name}"
+    else:
+        tea_str = f"{tea.name}"
+
+    filled_with = random.choice(["filled with", "of", "stuffed with",
+        "full of", "brimming with"])
+    return f"{vessel_str} {filled_with} {heat} {tea_str}"
+
+
+async def serve(self, ch, src, msg, args, opts):
+    """
+    :name: tea
+    :hook: cmd
+    :help: serve some tea!
+    :args: @user:str
+    """
+    recipient = src
+    if len(msg) > 0:
+        recipient = msg
+
+    tea = _tea()
+    action = random.choice(ACTION)
+
+    await self.ctcp(ch, "ACTION", f"{action} {recipient} a {tea}!")
+
+
+async def init(self):
+    handlers.register(self, modname, serve)
